1d8ae827054aa72298da547a324e71f9713344dc
hjk <hjkaddict@gmail.com>
2020-11-26 01:04:51 +0100

    =a

---
+++ app.js
@@ -0,0 +1,53 @@
+/*
+
+Todo:
+    - deploy nodapp to server (digital ocean)
+    - retreiving metadata in first connection
+
+
+*/
+
+
+const express = require('express')
+const path = require('path')
+
+const app = express()
+app.use(express.static('./'));
+
+const { Parser } = require('icecast-parser');
+
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+
+const radioStation = new Parser({
+    autoUpdate: true,
+    metadataInterval: 5,
+    notifyOnChangeOnly: false,
+    url: 'https://stream.oolongradio.com/oolong-radio'
+});
+
+
+io.on('connection', function (socket) {
+    radioStation.on('metadata', function (data) {
+        process.stdout.write(`${data.get('StreamTitle')}\n`);
+        socket.emit('metadata', { message: data.get('StreamTitle') });
+    })
+});
+
+app.get('/', async (req, res) => {
+    try {
+        res.render('index.html')
+    }
+    catch (e) {
+
+    }
+})
+
+
+server.listen(process.env.PORT || 3001, function () {
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
+
+// app.listen(process.env.PORT || 8080, function () {
+//     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+// });
