7c7a53d6e82bc197815eb934d282e8ab305ebe79
hjk <hjkaddict@gmail.com>
2020-11-26 13:21:42 +0100

    =a

--- app.js
+++ app.js
@@ -16,25 +16,25 @@ const publicDirectoryPath = path.join(__dirname, '../public')
 app.set('view engine', 'ejs')
 app.use(express.static('public'));
 
-// const { Parser } = require('icecast-parser');
+const { Parser } = require('icecast-parser');
 
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
-// const radioStation = new Parser({
-//     autoUpdate: true,
-//     metadataInterval: 5,
-//     notifyOnChangeOnly: false,
-//     url: 'https://stream.oolongradio.com/oolong-radio'
-// });
+const radioStation = new Parser({
+    autoUpdate: true,
+    metadataInterval: 5,
+    notifyOnChangeOnly: false,
+    url: 'https://stream.oolongradio.com/oolong-radio'
+});
 
 
-// io.on('connection', function (socket) {
-//     radioStation.on('metadata', function (data) {
-//         process.stdout.write(`${data.get('StreamTitle')}\n`);
-//         socket.emit('metadata', { message: data.get('StreamTitle') });
-//     })
-// });
+io.on('connection', function (socket) {
+    radioStation.on('metadata', function (data) {
+        process.stdout.write(`${data.get('StreamTitle')}\n`);
+        socket.emit('metadata', { message: data.get('StreamTitle') });
+    })
+});
 
 app.get('/', async (req, res) => {
     try {
