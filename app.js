de1cfc30126099c13943ccf8401e374aeafb054c
hjk <hjkaddict@gmail.com>
2020-11-26 03:08:26 +0100

    =a

--- app.js
+++ app.js
@@ -41,12 +41,9 @@ app.get('/', async (req, res) => {
         res.render('index')
     }
     catch (e) {
-        console.log(e)
+        console.log(e);
     }
 })
-
-
-// server.listen(3001);
 
 server.listen(process.env.PORT || 3001, function () {
     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
