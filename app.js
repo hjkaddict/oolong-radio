0c1e1be2cccfa9d28ae6c46e81f408d261231756
hjk <hjkaddict@gmail.com>
2020-12-02 23:38:45 +0100

    =a

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
 /*
 
 Todo:
-    - deploy nodapp to server (digital ocean)
     - retreiving metadata in first connection
-
+    - http request API
+    - liquidsoap: random proportion
 
 */
 
 const express = require('express')
 const path = require('path')
 const ejs = require('ejs')
+
+const unirest = require('unirest')
+const randomInt = require('random-int');
 
 const app = express()
 const publicDirectoryPath = path.join(__dirname, '../public')
@@ -21,20 +24,45 @@ const { Parser } = require('icecast-parser');
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
+//set radiostation option 
 const radioStation = new Parser({
     autoUpdate: true,
-    metadataInterval: 5,
+    metadataInterval: 2,
     notifyOnChangeOnly: false,
     url: 'https://stream.oolongradio.com/oolong-radio'
 });
 
-
+//socket.io
 io.on('connection', function (socket) {
     radioStation.on('metadata', function (data) {
-        // process.stdout.write(`${data.get('StreamTitle')}\n`);
         socket.emit('metadata', { message: data.get('StreamTitle') });
     })
 });
+
+//timer
+function intervalFunc() {
+    console.log(randomInt(-2, 2))
+}
+
+// setInterval(intervalFunc, 10000);
+
+
+//req for weather API
+var req = unirest("GET", "https://weatherbit-v1-mashape.p.rapidapi.com/current");
+
+// req
+//     .query({
+//         "lon": "13.42",
+//         "lat": "52.54"
+//     }).headers({
+//         "x-rapidapi-key": "1ff5a38105mshe44cd67aa91441ap16ec1fjsn37acebe33558",
+//         "x-rapidapi-host": "weatherbit-v1-mashape.p.rapidapi.com",
+//         "useQueryString": true
+//     }).end(function (res) {
+//         if (res.error) throw new Error(res.error);
+//         console.log(res.body.data[0].temp);         //temperature
+//         console.log(res.body.data[0].weather.code)  //weather
+//     });
 
 app.get('/', async (req, res) => {
     try {
---
+++ public/js/jPlayerFunc.js
@@ -0,0 +1,42 @@
+$(document).ready(function () {
+
+    var stream = {
+        mp3: "https://stream.oolongradio.com/oolong-radio"
+    },
+        ready = false;
+
+    $("#jquery_jplayer_1").jPlayer({
+        ready: function (event) {
+            ready = true;
+            $(this).jPlayer("setMedia", stream);
+        },
+        pause: function () {
+            $(this).jPlayer("clearMedia");
+        },
+        error: function (event) {
+            if (ready && event.jPlayer.error.type === $.jPlayer.error.URL_NOT_SET) {
+                // Setup the media stream again and play it.
+                $(this).jPlayer("setMedia", stream).jPlayer("play");
+            }
+        },
+
+        supplied: "mp3",
+        preload: "metadata",
+
+        waiting: function (event) {
+            // console.log('start loading stream')
+            $('.jp-pause').hide();
+            $("#loading").fadeIn(1000);
+        },
+        playing: function (event) {
+            //console.log('stream is successfully loaded')
+            $('.jp-pause').show();
+            $('#loading').hide();
+        }
+    });
+
+    $("#jquery_jplayer_1").jPlayer("volume", 0.8);
+    $(document).on('input', '#volumeSlider', function () {
+        $("#jquery_jplayer_1").jPlayer("volume", $(this).val());
+    });
+});
